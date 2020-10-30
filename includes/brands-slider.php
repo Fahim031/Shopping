459cb447b8583988a6af6418af1e9d190df278f3
Fahim031 <72388881+Fahim031@users.noreply.github.com>
2020-10-30 11:33:37 +0600

    Shopping

---
+++ includes/brands-slider.php
@@ -0,0 +1,37 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">PAY BY</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/aoc.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/bkashh.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/rockett.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/mastercardd.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
